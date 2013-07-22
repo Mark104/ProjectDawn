ef5aa87e578a48cf434bdcd7b1b70b286a292188
Mark104 <markdawson104@gmail.com>
2013-07-22 08:19:42 +0100

    Home Commits

--- ProjectDawn/Server/Assets/Game/Settings/Settings.cs
+++ ProjectDawn/Server/Assets/Game/Settings/Settings.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public static class Settings{
 	
-	public const string ServerIP = "192.168.0.2";
+	public const string ServerIP = "25.150.103.245";
 	//public const string HamachiServerIP = "25.150.103.245";
 	//public const string ExternalServerIP = "";
 	
